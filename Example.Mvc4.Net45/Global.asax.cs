0c05140fbf000114159e6ea58f40c0506d97c966
Richard Oliver <richard@rholiver.co.uk>
2013-04-25 20:58:59 +0100

    Added titles to web sites

--- Example.Mvc4.Net45/Global.asax.cs
+++ Example.Mvc4.Net45/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -12,10 +9,13 @@ namespace Example.Mvc4.Net45
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
-        protected void Application_Start()
-        {
+        public static string Title {
+            get { return "Example.Mvc4.Net45"; }
+        }
+
+        protected void Application_Start() {
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);

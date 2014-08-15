7c1e59a39f71c437a2bbd52f2ceefa851fe75646
Pierre setteskog <pierre.setteskog@gmail.com>
2014-08-15 08:53:39 +0200

    Update HtmlResource.cs
    fixed breaking change

--- Glimpse.NLog/Resource/HtmlResource.cs
+++ Glimpse.NLog/Resource/HtmlResource.cs
@@ -9,13 +9,16 @@ namespace Glimpse.NLog.Resource
     {
         private const string InternalName = "glimpse_nlog_html";
 
+        protected override EmbeddedResourceInfo GetEmbeddedResourceInfo(IResourceContext context)
+        {
+            return new EmbeddedResourceInfo(this.GetType().Assembly, "Glimpse.NLog.Resource.glimpse.nlog.html", @"text/html");
+        }
+        
         public override IEnumerable<ResourceParameterMetadata> Parameters {
             get { return Enumerable.Empty<ResourceParameterMetadata>(); }
         }
 
         public HtmlResource() {
-            ResourceName = "Glimpse.NLog.Resource.glimpse.nlog.html";
-            ResourceType = @"text/html";
             Name = InternalName;
         }
 
